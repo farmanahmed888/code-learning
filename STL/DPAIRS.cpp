f77faec77226bd9d0e2ae477f61aeed74d3dabf1
Farman Ahmed <farmanahmed@Farmans-MacBook-Air.local>
2022-03-29 11:31:00 +0530

    add files

--- STL/DPAIRS.cpp
+++ STL/DPAIRS.cpp
@@ -1,37 +1,32 @@
-<html>
-                    <head>
-                        <meta name='robots' content='noindex, nofollow' />
-                    </head>
-                    <body>
-                        <pre>#include &lt;bits/stdc++.h&gt;
+#include <bits/stdc++.h>;
 using namespace std;
 void solve(){
 	int n,m;
-	cin&gt;&gt;n&gt;&gt;m;
-	std::vector&lt;pair&lt;int,int&gt; &gt; a;
-	std::vector&lt;pair&lt;int,int&gt; &gt; b;
-	for(int i = 0; i &lt; n; i++){
+	cin>>n>>m;
+	std::vector<pair<int,int> > a;
+	std::vector<pair<int,int> > b;
+	for(int i = 0; i < n; i++){
 		int input;
-		cin&gt;&gt;input;
+		cin>>;input;
 		a.push_back(make_pair(input,i));
 	}
-	for(int i = 0; i &lt; m; i++){
+	for(int i = 0; i < m; i++){
 		int input;
-		cin&gt;&gt;input;
+		cin>>input;
 		b.push_back(make_pair(input,i));
 	}
-	std::vector&lt;pair&lt;int,int&gt; &gt; v;	
+	std::vector<pair<int,int> > v;	
 	sort(a.begin(), a.end());
 	sort(b.begin(), b.end());
-	for(int i = 0; i &lt; m; i++){
+	for(int i = 0; i < m; i++){
 		v.push_back(make_pair(a[0].second,b[i].second));
 	}
-	for(int i = 1; i &lt; n; i++){
+	for(int i = 1; i < n; i++){
 		v.push_back(make_pair(a[i].second,b[m-1].second));
 	}
 	
-	for(int i = 0; i &lt; n+m-1; i++){
-		cout&lt;&lt;v[i].first&lt;&lt;&quot; &quot;&lt;&lt;v[i].second&lt;&lt;&quot;\n&quot;;
+	for(int i = 0; i < n+m-1; i++){
+		cout<<v[i].first<<" "<<v[i].second<<"\n";
 	}
 	
 }
@@ -39,6 +34,3 @@ int main(){
 	solve();
 	return 0;
 }
-</pre>
-                    </body>
-                </html>
