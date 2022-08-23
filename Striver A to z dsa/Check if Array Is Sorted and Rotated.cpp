666e3f6a90009ea82fb6b54b527cb971103d333c
Farman Ahmed <farmanahmed888@gmail.com>
2022-08-23 21:39:44 +0530

    add files

--- Striver A to z dsa/Check if Array Is Sorted and Rotated.cpp
+++ Striver A to z dsa/Check if Array Is Sorted and Rotated.cpp
@@ -12,3 +12,14 @@ public:
         return true;
     }
 };
+/*
+Compare all neignbour elements (a,b) in A,
+the case of a > b can happen at most once.
+
+Note that the first element and the last element are also connected.
+
+If all a <= b, A is already sorted.
+If all a <= b but only one a > b,
+we can rotate and make b the first element.
+Other case, return false.
+*/
