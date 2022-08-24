45011ac8b238efba9ff6ec7e53b1eda0ecc641af
Farman Ahmed <farmanahmed888@gmail.com>
2022-08-24 21:54:36 +0530

    add files

---
+++ Striver A to z dsa/Single Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xo = 0;
+        for(auto x : nums){
+            xo = xo ^ x;
+        }
+        return xo;
+    }
+};
